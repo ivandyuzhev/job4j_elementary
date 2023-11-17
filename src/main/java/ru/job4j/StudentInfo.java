73c65b708f460ca40a26f011d0f61e8c6cc15ce9
ivandyuzhev <axensk@yandex.ru>
2023-11-17 23:46:43 +0700

    4.1. Разбор первой программы. [#159033 #459837]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-public static void main (String[] args){
-    System.out.println("Ivan Dyuzhev");
-    System.out.println("12.02.1994");
-}
+    public static void main(String[] args) {
+        System.out.println("Ivan Dyuzhev");
+        System.out.println("12.02.1994");
+    }
 }
