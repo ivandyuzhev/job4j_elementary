339bb5f75e0414b38c042246b6099d8d9ae8fc02
ivandyuzhev <axensk@yandex.ru>
2023-11-30 12:50:04 +0700

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int oneNum, int twoNum) {
+        int result = oneNum >= twoNum ? oneNum : twoNum;
+        return result;
+    }
+
+    public static int max(int oneNum, int twoNum, int threeNum) {
+        return max(oneNum, max(twoNum, threeNum));
+    }
+
+    public static int max(int oneNum, int twoNum, int threeNum, int fourNum) {
+        return max(oneNum, max(twoNum, threeNum, fourNum));
+    }
+}
