edce6f1bba2eb075d4070cd97a97a184cfce87c9
ivandyuzhev <axensk@yandex.ru>
2024-01-14 20:37:36 +0700

    4.3.3. Логическое отрицание ! [#334072 #463451]

--- src/test/java/ru/job4j/condition/LogicNotTest.java
+++ src/test/java/ru/job4j/condition/LogicNotTest.java
@@ -70,7 +70,7 @@ class LogicNotTest {
 
     @Test
     void whenNumIs0NotPositiveFalse() {
-        int num = 1;
+        int num = 0;
         boolean result = LogicNot.notPositive(num);
         assertThat(result).isFalse();
     }
