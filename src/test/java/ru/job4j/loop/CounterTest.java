28fb80991135b202a7982a11940a6deb1d953c5b
ivandyuzhev <axensk@yandex.ru>
2024-01-18 12:48:15 +0700

    5.0.1. Сумма чисел. [#226416]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromFourToTwentyOneThenOneHundredAndEight() {
+        int start = 3;
+        int finish = 12;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 40;
+        assertThat(result).isEqualTo(expected);
+    }
+}
