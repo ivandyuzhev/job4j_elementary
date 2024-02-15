dfa33cda5f6899e40f22692616da3088392d888d
ivandyuzhev <axensk@yandex.ru>
2024-02-15 11:40:54 +0700

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,12 +1,13 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int element = 5;
         int result = FindLoop.indexOf(data, element);
         int expected = 0;
@@ -15,7 +16,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHas8ThenResultEqualFinish() {
-        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int[] data = new int[]{5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
         int element = 8;
         int start = 2;
         int finish = 5;
@@ -26,7 +27,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHasSomeEqualElementThen3() {
-        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int[] data = new int[]{5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
         int element = 4;
         int start = 1;
         int finish = 8;
@@ -37,7 +38,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHas2Then3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
+        int[] data = new int[]{5, 2, 10, 2, 4};
         int element = 2;
         int start = 2;
         int finish = 4;
@@ -47,8 +48,22 @@ public class FindLoopTest {
     }
 
     @Test
+    void whenArrayHas17ThenNotFound() {
+        int[] data = {22, 16, 33, 28, 14, 66, 89};
+        int element = 17;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     public void whenDiapasonHasNot8ThenMinus1() {
-        /* Дописать тест, когда элемент 8 в диапазоне не найден.
-        Можно искать любой другой элемент, корректно отражая этот факт в названии теста. */
+        int[] data = {13, 15, 16, 2, 3, 7, 9, 1, 14, 28, 29, 23};
+        int element = 8;
+        int start = 4;
+        int finish = 10;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
     }
 }
