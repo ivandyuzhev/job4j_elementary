2d1f25026c0dc64e27a11fbf918f5a723c879caf
ivandyuzhev <axensk@yandex.ru>
2024-04-18 13:56:06 +0700

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -10,37 +10,19 @@ class PointTest {
         double expected = 2;
         Point point = new Point(0, 0);
         Point r1 = new Point(2, 0);
+        point.info();
         double out = point.distance(r1);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
 
     }
 
     @Test
-    void when50to56then6() {
+    void when500to560then6() {
         double expected = 6;
-        Point point = new Point(5, 0);
-        Point r2 = new Point(5, 6);
-        double out = point.distance(r2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
-
-    }
-
-    @Test
-    void when70to74then4() {
-        double expected = 4;
-        Point point = new Point(7, 0);
-        Point r3 = new Point(7, 4);
-        double out = point.distance(r3);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
-
-    }
-
-    @Test
-    void when40to44then8() {
-        double expected = 10;
-        Point point = new Point(0, 0);
-        Point r4 = new Point(6, 8);
-        double out = point.distance(r4);
+        Point point = new Point(5, 0, 0);
+        Point r2 = new Point(5, 6, 0);
+        double out = point.distance3d(r2);
+        point.info();
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
 
     }
